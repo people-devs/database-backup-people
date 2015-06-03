c4e2e772c74fbffb65ad2350e90178cea6c607d3
Dev @ PC <dev@pc.co.il>
2015-06-03 16:09:49 +0300

    backing up all apps

--- bin/backup.sh
+++ bin/backup.sh
@@ -8,10 +8,10 @@ if [[ -z "$APP" ]]; then
   exit 1
 fi
 
-if [[ -z "$DATABASE" ]]; then
-  echo "Missing DATABASE variable which must be set to the name of the DATABASE you would like to backup"
-  exit 1
-fi
+# if [[ -z "$DATABASE" ]]; then
+#   echo "Missing DATABASE variable which must be set to the name of the DATABASE you would like to backup"
+#   exit 1
+# fi
 
 if [[ -z "$S3_BUCKET_PATH" ]]; then
   echo "Missing S3_BUCKET_PATH variable which must be set the directory in s3 where you would like to store your database backups"
@@ -24,11 +24,27 @@ unzip awscli-bundle.zip
 chmod +x ./awscli-bundle/install
 ./awscli-bundle/install -i /tmp/aws
 
-BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.dump"
+#BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.dump"
+BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP.dump"
 
-/app/vendor/heroku-toolbelt/bin/heroku pg:backups capture $DATABASE --app $APP
-curl -o $BACKUP_FILE_NAME `/app/vendor/heroku-toolbelt/bin/heroku pg:backups public-url --app $APP`
-gzip $BACKUP_FILE_NAME
-/tmp/aws/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://$S3_BUCKET_PATH/$APP/$DATABASE/$BACKUP_FILE_NAME.gz
-echo "backup $BACKUP_FILE_NAME complete"
+apps=( `heroku apps` )
+echo "Total number of apps :"  $(( ${#apps[@]} -3 )) 
+for (( i = 3 ; i < ${#apps[@]} ; i++ ))
+do
+  if [[ ${apps[$i]} =~ "backup" ]] ; then
+    echo "no need to backup ${apps[$i]}!"
+  else
+    echo ${apps[$i]}
+    /app/vendor/heroku-toolbelt/bin/heroku pg:backups capture $DATABASE --app ${apps[$i]}
+    curl -o $BACKUP_FILE_NAME `/app/vendor/heroku-toolbelt/bin/heroku pg:backups public-url --app ${apps[$i]}`
+    gzip $BACKUP_FILE_NAME
+    /tmp/aws/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://$S3_BUCKET_PATH/$APP/$DATABASE/$BACKUP_FILE_NAME.gz
+    echo "backup $BACKUP_FILE_NAME complete"
+  fi
+done
+# /app/vendor/heroku-toolbelt/bin/heroku pg:backups capture $DATABASE --app $APP
+# curl -o $BACKUP_FILE_NAME `/app/vendor/heroku-toolbelt/bin/heroku pg:backups public-url --app $APP`
+# gzip $BACKUP_FILE_NAME
+# /tmp/aws/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://$S3_BUCKET_PATH/$APP/$DATABASE/$BACKUP_FILE_NAME.gz
+# echo "backup $BACKUP_FILE_NAME complete"
 
