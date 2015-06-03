0524b8d460733b219f71bcfb4bccf1d90b956a76
Dev @ PC <dev@pc.co.il>
2015-06-03 16:50:30 +0300

    removed gz extension from filename

--- bin/backup.sh
+++ bin/backup.sh
@@ -38,7 +38,7 @@ do
     /app/vendor/heroku-toolbelt/bin/heroku pg:backups capture --app ${apps[$i]}
     curl -o $BACKUP_FILE_NAME `/app/vendor/heroku-toolbelt/bin/heroku pg:backups public-url --app ${apps[$i]}`
     #gzip $BACKUP_FILE_NAME
-    /tmp/aws/bin/aws s3 cp $BACKUP_FILE_NAME.gz s3://$S3_BUCKET_PATH/${apps[$i]}/$BACKUP_FILE_NAME
+    /tmp/aws/bin/aws s3 cp $BACKUP_FILE_NAME s3://$S3_BUCKET_PATH/${apps[$i]}/$BACKUP_FILE_NAME
     echo "backup $BACKUP_FILE_NAME complete"
   fi
 done
